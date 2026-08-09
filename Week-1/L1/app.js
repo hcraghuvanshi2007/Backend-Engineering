95c10850692fe89ca21cf863a2868709727621dc
Himanchal <hcraghuvanshi2007@gmail.com>
2026-08-09 22:39:14 +0530

    global object

--- Week-1/L1/app.js
+++ Week-1/L1/app.js
@@ -1,1 +1,7 @@
+// => Global object
+
+//console.log(window); // Error => window is not defined in node.js
+//1st
 console.log(global);
+//2nd
+global.console.log("Hey Buddy");
---
+++ Week-1/L1/app1.js
@@ -0,0 +1,4 @@
+// globalThis
+globalThis.console.log("Hello Again!!!")
+console.log(globalThis) // same as console.log(global) => Global
+global.console.log("kemchooo???")
