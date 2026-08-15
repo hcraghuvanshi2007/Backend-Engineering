ffaa734c49bfa62cce734ed3a194c0188b17f138
Himanchal <hcraghuvanshi2007@gmail.com>
2026-08-15 21:01:23 +0530

    new file math.js with module exports

---
+++ Week-1/L4/math.js
@@ -0,0 +1,8 @@
+function add(a,b){
+    return a+b;
+}
+
+//module.exports = "Rounak"; // will export Rounak to other files.
+
+module.exports = add(8,9); // will export add function to other files.
+
