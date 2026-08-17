01a8caf13610e4cd088893513207cc2430f1cab1
Himanchal <hcraghuvanshi2007@gmail.com>
2026-08-17 20:24:42 +0530

    module exports types

--- Week-2/L5/math.js
+++ Week-2/L5/math.js
@@ -4,6 +4,14 @@ function add(a,b){
 function sub(c,d){
     return c-d;
 }
+// Export Type-1
 module.exports = add(8,9);
 module.exports = sub(8,9);
-
+// Export Type-2
+module.exports = "1";
+module.exports = "2"; // This value will overwritten the first value. # Output=2
+// Export Type-3
+module.exports = {
+    add1:add,
+    sub1:sub
+}
