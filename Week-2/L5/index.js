d82e3dbaec31d4b79b6769afa229d386a78094bf
Himanchal <hcraghuvanshi2007@gmail.com>
2026-08-17 20:36:19 +0530

    module exports and imports

--- Week-2/L5/index.js
+++ Week-2/L5/index.js
@@ -1,3 +1,11 @@
-const math = require('./math');
+//const math = require('./math');
+const {add,sub} = require('./math'); // For Export types 3
 
-console.log("Math Value is:",math);
+
+//console.log("Math Value is:",math); // For Export types 1,2
+
+//console.log("Math Value is:",math.add1(2,8)); // For Export types 4
+//console.log("Math Value is:",math.sub1(8,2)); // For Export types 4
+//AlSO
+console.log("Math Value is:",add(2,8)); // For Export types 3
+console.log("Math Value is:",sub(8,2)); // For Export types 3
--- Week-2/L5/math.js
+++ Week-2/L5/math.js
@@ -12,6 +12,11 @@ module.exports = "1";
 module.exports = "2"; // This value will overwritten the first value. # Output=2
 // Export Type-3
 module.exports = {
+    add,
+    sub
+}
+// Export Type-4
+module.exports = {
     add1:add,
     sub1:sub
 }
