5bb5d0859feeb910159536bea838464273ab2cd7
Himanchal <hcraghuvanshi2007@gmail.com>
2026-08-19 21:32:50 +0530

    fs file stats and directory creation

---
+++ Week-2/L8/file4.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+//fs.writeFile('./unlink.txt','Heyyy',(err)=>{})
+
+// Delete a file
+
+//fs.unlinkSync("./unlink.txt");
+
+// Statistic of File
+console.log(fs.statSync("./test.txt"));
+console.log(fs.statSync("./test.txt").isFile());
+
+// Make a directory
+fs.mkdirSync("my-docs");
